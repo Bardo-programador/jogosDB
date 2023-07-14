be8687ab0a5888093656eca723ffa02eb992f3e5
Samuel Roberto <66800900+Bardo-programador@users.noreply.github.com>
2023-07-14 11:58:52 -0300

    Update Steam.py

--- pages/Steam.py
+++ pages/Steam.py
@@ -11,7 +11,7 @@ def run(STRING):
 st.title("Promoções da Steam")
 
 # Executar a raspagem ao clicar no botão
-# cmdline.execute("scrapy crawl steam_especiais".split())
-run('scrapy crawl steam_especiais')
+cmdline.execute("scrapy crawl steam_especiais".split())
+# run('scrapy crawl steam_especiais')
 steam = pd.read_json("dados/steam.jsonl", lines=True)
 st.dataframe(steam)
