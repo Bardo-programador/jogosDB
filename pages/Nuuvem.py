d11dbf7086f9a68194f121c92331de44f7815b3c
Samuel Roberto <samucadeus23@gmail.com>
2023-08-18 11:42:37 -0300

    Exibição usando markdown e opçao de link no nome

--- pages/Nuuvem.py
+++ pages/Nuuvem.py
@@ -28,4 +28,9 @@ with open("dados/agenda.txt", 'r') as agenda:
             agenda.write(str(nova_agenda + datetime.timedelta(days=1)))
     
 nuuvem = pd.read_json("dados/nuuvem.jsonl", lines=True) ##Lê o arquivo jsonl
-st.dataframe(nuuvem) ## Exibe o dataframe
+for index, row in nuuvem.iterrows():
+    nome = row['name'] # Como o nome é uma lista, pegamos o primeiro elemento
+    preco = row['price'] # Como o preço é uma lista, pegamos o primeiro elemento
+    link = row['link'] # O link não é uma lista, então não precisamos pegar o primeiro elemento
+    st.markdown(f"- [{nome}]({link}) - R$ {preco}") # Exibimos o nome, o link e o preço formatado em MarkDown
+
--- pages/Steam.py
+++ pages/Steam.py
@@ -9,4 +9,12 @@ st.title("Promoções da Steam")
 # Executar a raspagem ao clicar no botão
 run('scrapy crawl steam_especiais')
 steam = pd.read_json("dados/steam.jsonl", lines=True)
-st.dataframe(steam)
+
+##Exibir os dados
+
+st.markdown("## Lista de jogos em promoção da Steam")
+for index, row in steam.iterrows():
+    nome = row['name'][0] # Como o nome é uma lista, pegamos o primeiro elemento
+    preco = row['price'][0] # Como o preço é uma lista, pegamos o primeiro elemento
+    link = row['link'] # O link não é uma lista, então não precisamos pegar o primeiro elemento
+    st.markdown(f"- [{nome}]({link}) - USD$ {preco}") # Exibimos o nome, o link e o preço formatado em MarkDown
