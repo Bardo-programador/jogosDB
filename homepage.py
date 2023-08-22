fd86597d0069ee145ca5f1e5e86850159be0b99e
Samuel Roberto <samucadeus23@gmail.com>
2023-08-22 16:24:12 -0300

    FEAT: homepage com mais informações

--- homepage.py
+++ homepage.py
@@ -1,5 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title='Jogos', page_icon=':video_game:', layout='wide', initial_sidebar_state='auto')
-st.title('Jogos')
+st.set_page_config(page_title='JogosDB', page_icon=':video_game:', layout='wide', initial_sidebar_state='auto')
+st.title('JogosDB')
+
+st.markdown("""
+## Bem-vindo ao JogosDB, aqui você vê promoções de seus jogos favoritos!
+Atualmente as lojas suportadas são:
+- [Nuuvem](https://www.nuuvem.com/)
+- [Steam](https://store.steampowered.com/)
+""")
