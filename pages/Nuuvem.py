b5db3a05b06f1e2e2690e03f5601d324ddc103e5
Samuel Roberto <samucadeus23@gmail.com>
2023-08-22 16:23:00 -0300

    FEAT: Barra de navegação adicionada

--- pages/Nuuvem.py
+++ pages/Nuuvem.py
@@ -10,7 +10,7 @@ try:
 except:
     pass
 st.title("Promoções da Nuuvem")
-
+# Raspagem de dados e cronometro até a próxima raspagem
 with open("dados/agenda.txt", 'r') as agenda:
     hora_agendada = agenda.read() ##Lê a hora agendada
 if hora_agendada == "": ##Se estiver vazio, agenda para o dia seguinte
@@ -26,11 +26,15 @@ if hora_agendada <= datetime.datetime.now(): ##Se a hora agendada for menor que 
             with open('dados/agenda.txt', 'w') as agenda: ##Salva a nova agenda para o dia seguinte as 00:00
                 nova_agenda = datetime.datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
                 agenda.write(str(nova_agenda + datetime.timedelta(days=1)))
-    
+
+# Exibição dos dados    
 nuuvem = pd.read_json("dados/nuuvem.jsonl", lines=True) ##Lê o arquivo jsonl
+filtro = st.text_input("Procure por um jogo") ##Cria um filtro para o nome do jogo
 for index, row in nuuvem.iterrows():
-    nome = row['name'] # Como o nome é uma lista, pegamos o primeiro elemento
-    preco = row['price'] # Como o preço é uma lista, pegamos o primeiro elemento
-    link = row['link'] # O link não é uma lista, então não precisamos pegar o primeiro elemento
-    st.markdown(f"- [{nome}]({link}) - R$ {preco}") # Exibimos o nome, o link e o preço formatado em MarkDown
+    nome_minusculo = row['name'].lower() ##Transforma o nome do jogo em minusculo
+    if nome_minusculo.find(filtro) != -1:
+        nome = row['name'] # Como o nome é uma lista, pegamos o primeiro elemento
+        preco = row['price'] # Como o preço é uma lista, pegamos o primeiro elemento
+        link = row['link'] # O link não é uma lista, então não precisamos pegar o primeiro elemento
+        st.markdown(f"- [{nome}]({link}) - R$ {preco}") # Exibimos o nome, o link e o preço formatado em MarkDown
 
--- pages/Steam.py
+++ pages/Steam.py
@@ -12,10 +12,12 @@ with st.spinner("Atualizando"):
 steam = pd.read_json("dados/steam.jsonl", lines=True)
 
 ##Exibir os dados
-
+filtro = st.text_input("Procure por um jogo") ##Cria um filtro para o nome do jogo
 st.markdown("## Lista de jogos em promoção da Steam")
 for index, row in steam.iterrows():
-    nome = row['name'][0] # Como o nome é uma lista, pegamos o primeiro elemento
-    preco = row['price'][0] # Como o preço é uma lista, pegamos o primeiro elemento
-    link = row['link'] # O link não é uma lista, então não precisamos pegar o primeiro elemento
-    st.markdown(f"- [{nome}]({link}) - USD$ {preco}") # Exibimos o nome, o link e o preço formatado em MarkDown
+    nome_minusculo = row['name'][0].lower() ##Transforma o nome do jogo em minusculo
+    if nome_minusculo.find(filtro.lower()) != -1:
+        nome = row['name'][0] # Como o nome é uma lista, pegamos o primeiro elemento
+        preco = row['price'][0] # Como o preço é uma lista, pegamos o primeiro elemento
+        link = row['link'] # O link não é uma lista, então não precisamos pegar o primeiro elemento
+        st.markdown(f"- [{nome}]({link}) - USD$ {preco}") # Exibimos o nome, o link e o preço formatado em MarkDown
