707817227664aa64a12503d6123311c7c1fb2660
Samuel Roberto <samucadeus23@gmail.com>
2024-04-14 14:44:09 -0300

    JsonWriterPipeline removida

--- jogosDB/pipelines.py
+++ jogosDB/pipelines.py
@@ -13,18 +13,7 @@ class JogosdbPipeline:
     def process_item(self, item, spider):
         return item
 
-class JsonWriterPipeline:
-    def open_spider(self, spider):
-        self.file = open("items.jsonl", "w")
-
-    def close_spider(self, spider):
-        self.file.close()
-
-    def process_item(self, item, spider):
-        line = json.dumps(ItemAdapter(item).asdict()) + "\n"
-        self.file.write(line)
-        return item
-    
+   
 class SteamWriterPipeline(JsonWriterPipeline):
     def open_spider(self, spider):
         if os.path.exists("dados"):
